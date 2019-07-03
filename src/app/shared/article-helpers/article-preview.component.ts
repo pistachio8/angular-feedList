008bc1139eed9e2810b95776bf140f5cb2fee11b
pistachio8 <wjaden0107@gmail.com>
2019-07-03 17:25:08 +0900

    공유 모듈 생성

---
+++ src/app/shared/article-helpers/article-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-preview',
+  templateUrl: './article-preview.component.html',
+  styles: []
+})
+export class ArticlePreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
